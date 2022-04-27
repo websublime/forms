bf932f6877eed6ee1d627f9e83d079dbf4c48a41
Hulago <Hulago@users.noreply.github.com>
2022-04-27 15:06:48 +0000

    fix: fix home page documentation (#7)
    Co-authored-by: Hugo Rodrigues <hugorodrigues@prozis.com> afadad698ca3b64ee4c221ef4e34ff8ef4fdec78

---
+++ assets/api_forms.formcontrolcomponent.md.28dae366.js
@@ -0,0 +1,23 @@
+import{_ as n,c as a,o as s,a as t}from"./app.2931ab38.js";const f='{"title":"FormControlComponent variable","description":"","frontmatter":{},"headers":[{"level":2,"title":"FormControlComponent variable","slug":"formcontrolcomponent-variable"}],"relativePath":"api/forms.formcontrolcomponent.md"}',o={},p=t(`<p><a href="./">Home</a> &gt; <a href="./forms.html">@websublime/forms</a> &gt; <a href="./forms.formcontrolcomponent.html">FormControlComponent</a></p><h2 id="formcontrolcomponent-variable" tabindex="-1">FormControlComponent variable <a class="header-anchor" href="#formcontrolcomponent-variable" aria-hidden="true">#</a></h2><p><b>Signature:</b></p><div class="language-typescript"><pre><code>FormControlComponent<span class="token operator">:</span> <span class="token punctuation">{</span>
+    computed<span class="token operator">:</span> <span class="token punctuation">{</span>
+        <span class="token function">errors</span><span class="token punctuation">(</span><span class="token punctuation">)</span><span class="token operator">:</span> <span class="token builtin">string</span><span class="token punctuation">[</span><span class="token punctuation">]</span><span class="token punctuation">;</span>
+    <span class="token punctuation">}</span><span class="token punctuation">;</span>
+    methods<span class="token operator">:</span> <span class="token punctuation">{</span>
+        <span class="token function">blur</span><span class="token punctuation">(</span>event<span class="token operator">:</span> <span class="token builtin">any</span><span class="token punctuation">)</span><span class="token operator">:</span> <span class="token keyword">void</span><span class="token punctuation">;</span>
+        <span class="token function">focus</span><span class="token punctuation">(</span>event<span class="token operator">:</span> <span class="token builtin">any</span><span class="token punctuation">)</span><span class="token operator">:</span> <span class="token keyword">void</span><span class="token punctuation">;</span>
+        <span class="token function">input</span><span class="token punctuation">(</span>event<span class="token operator">:</span> <span class="token builtin">any</span><span class="token punctuation">)</span><span class="token operator">:</span> <span class="token keyword">void</span><span class="token punctuation">;</span>
+    <span class="token punctuation">}</span><span class="token punctuation">;</span>
+    name<span class="token operator">:</span> <span class="token builtin">string</span><span class="token punctuation">;</span>
+    props<span class="token operator">:</span> <span class="token punctuation">{</span>
+        formControl<span class="token operator">:</span> <span class="token punctuation">{</span>
+            <span class="token function-variable function">default</span><span class="token operator">:</span> <span class="token punctuation">(</span><span class="token punctuation">)</span> <span class="token operator">=&gt;</span> <span class="token keyword">null</span><span class="token punctuation">;</span>
+            type<span class="token operator">:</span> ObjectConstructor<span class="token punctuation">;</span>
+        <span class="token punctuation">}</span><span class="token punctuation">;</span>
+        modelEvent<span class="token operator">:</span> <span class="token punctuation">{</span>
+            <span class="token keyword">default</span><span class="token operator">:</span> <span class="token builtin">string</span><span class="token punctuation">;</span>
+            type<span class="token operator">:</span> StringConstructor<span class="token punctuation">;</span>
+        <span class="token punctuation">}</span><span class="token punctuation">;</span>
+    <span class="token punctuation">}</span><span class="token punctuation">;</span>
+    <span class="token function">render</span><span class="token punctuation">(</span><span class="token punctuation">)</span><span class="token operator">:</span> <span class="token builtin">any</span><span class="token punctuation">;</span>
+<span class="token punctuation">}</span>
+</code></pre></div>`,4),e=[p];function c(l,r,u,i,k,m){return s(),a("div",null,e)}var _=n(o,[["render",c]]);export{f as __pageData,_ as default};
