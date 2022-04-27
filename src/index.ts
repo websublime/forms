afadad698ca3b64ee4c221ef4e34ff8ef4fdec78
Hugo Lago <huzgo1@gmail.com>
2022-04-27 16:05:52 +0100

    fix: fix home page documentation (#7)
    Co-authored-by: Hugo Rodrigues <hugorodrigues@prozis.com>

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { BaseControl } from './base-control';
 export { FormArray } from './form-array';
 export { FormControl } from './form-control';
 export { FormGroup } from './form-group';
+export { FormControlComponent } from './form-control.component';
